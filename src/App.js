d1eb6b209431c87ce26de229fea978de6a32c4c3
Sinf4er <1f2@xxxlutz.at>
2019-11-08 08:51:25 +0100

    optimize hashing for htmlFor

--- src/App.js
+++ src/App.js
@@ -36,19 +36,19 @@ class App extends React.Component {
     const counterColor = classNames(style.counter, {
       [style.red]: this.state.charsLeft < 0
     });
-
+    
     return (
       <div className={style.app}>
         <header className={style.header}>
           <div className={style.text}>
             <div className={counterColor}>500/{this.state.charsLeft} Left</div>
-            <label htmlFor="this.hash">Caracter Counter</label>
+            <label htmlFor={this.hash}>Caracter Counter</label>
           </div>
           <textarea
             onChange={this.handleChange}
             className={style.input}
             name="text"
-            id="this.hash"
+            id={this.hash}
             type="text"
             placeholder="Enter some text here..."
           ></textarea>
